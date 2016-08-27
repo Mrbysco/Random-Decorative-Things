43944c1b218573a017e5029b0e17ccb5008b9a23
Mrbysco <Heeybysco@live.nl>
2016-08-27 02:37:28 +0200

    Soon to be miab

--- src/main/java/com/Mrbysco/RDT/RDTReference.java
+++ src/main/java/com/Mrbysco/RDT/RDTReference.java
@@ -5,6 +5,7 @@ public class RDTReference {
 	public static final String MOD_NAME = "Random Decorative Things";
 	public static final String VERSION = "0.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.10.2]";
+	public static final String DEPENDENCIES = "after:miab;";
 			
 	public static final String CLIENT_PROXY_CLASS = "com.Mrbysco.RDT.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.Mrbysco.RDT.proxy.ServerProxy";
--- src/main/java/com/Mrbysco/RDT/RandomDecorativeThings.java
+++ src/main/java/com/Mrbysco/RDT/RandomDecorativeThings.java
@@ -1,9 +1,13 @@
 package com.Mrbysco.RDT;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.Mrbysco.RDT.init.RDTItems;
 import com.Mrbysco.RDT.init.RDTRecipes;
 import com.Mrbysco.RDT.proxy.CommonProxy;
 
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -12,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = RDTReference.MOD_ID, name = RDTReference.MOD_NAME, version = RDTReference.VERSION, acceptedMinecraftVersions = RDTReference.ACCEPTED_VERSIONS)
+@Mod(modid = RDTReference.MOD_ID, name = RDTReference.MOD_NAME, version = RDTReference.VERSION, acceptedMinecraftVersions = RDTReference.ACCEPTED_VERSIONS, dependencies = RDTReference.MOD_ID)
 public class RandomDecorativeThings
 {
 	
@@ -22,11 +26,22 @@ public class RandomDecorativeThings
 	@SidedProxy(clientSide = RDTReference.CLIENT_PROXY_CLASS, serverSide = RDTReference.SERVER_PROXY_CLASS)
 	public static CommonProxy proxy;
 	
+	public static final Logger logger = LogManager.getLogger(RDTReference.MOD_ID);
+	public static boolean isMiabEnabled = false;
+	
 	public static final RDTTab tabRDT = new RDTTab("tabRDT");
 	
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event)
     {
+    	isMiabEnabled = Loader.isModLoaded("miab");
+		if(isMiabEnabled)logger.info("Loading With Meme In A Bottle");
+		else{logger.info("Loading Without Meme In A Bottle");}
+
+		if(isMiabEnabled){
+			
+		}
+		
     	RDTItems.init();
     	RDTItems.register();
     	RDTRecipes.register();
