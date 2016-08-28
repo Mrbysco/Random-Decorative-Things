1c0825c111ed0b920e256f8aab170c1c83e7b2ff
Mrbysco <Heeybysco@live.nl>
2016-08-28 23:04:13 +0200

    Reversal recipe added

--- src/main/java/com/Mrbysco/RDT/init/RDTRecipes.java
+++ src/main/java/com/Mrbysco/RDT/init/RDTRecipes.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class RDTRecipes {
 	
 	public static void register() {
+		//Wool into woolbits
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 15), "W", 'W', new ItemStack(Blocks.WOOL, 1, 0));
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 14), "W", 'W', new ItemStack(Blocks.WOOL, 1, 1));
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 13), "W", 'W', new ItemStack(Blocks.WOOL, 1, 2));
@@ -23,6 +24,23 @@ public class RDTRecipes {
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 2), "W", 'W', new ItemStack(Blocks.WOOL, 1, 13));
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 1), "W", 'W', new ItemStack(Blocks.WOOL, 1, 14));
 		GameRegistry.addShapedRecipe(new ItemStack(RDTItems.woolbit, 8, 0), "W", 'W', new ItemStack(Blocks.WOOL, 1, 15));
-
+		
+		//woolbits into Wool (revert method)
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 0), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 15));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 1), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 14));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 2), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 13));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 3), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 12));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 4), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 11));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 5), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 10));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 6), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 9));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 7), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 8));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 8), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 7));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 9), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 6));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 10), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 5));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 11), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 4));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 12), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 3));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 13), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 2));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 14), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 1));
+		GameRegistry.addShapedRecipe(new ItemStack(Blocks.WOOL, 1, 15), "WWW","W W","WWW", 'W', new ItemStack(RDTItems.woolbit, 8, 0));
 	}
 }
