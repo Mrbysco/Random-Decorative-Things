f7981a292da80b0616939534dccb5bcc260a4600
Mrbysco <Heeybysco@live.nl>
2016-11-09 01:49:12 +0100

    --

--- src/main/java/com/Mrbysco/RDT/blocks/villages/BlockVillageHut2.java
+++ src/main/java/com/Mrbysco/RDT/blocks/villages/BlockVillageHut2.java
@@ -2,6 +2,8 @@ package com.Mrbysco.RDT.blocks.villages;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.Mrbysco.RDT.RDTReference;
 import com.Mrbysco.RDT.RandomDecorativeThings;
 import com.Mrbysco.RDT.blocks.BlockVillageBasic;
@@ -41,6 +43,5 @@ public class BlockVillageHut2 extends BlockVillageBasic{
     public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox,
     		List<AxisAlignedBB> collidingBoxes, Entity entityIn) {
     	super.addCollisionBoxToList(state, worldIn, pos, entityBox, collidingBoxes, entityIn);
-    }
-  
+    } 
 }
