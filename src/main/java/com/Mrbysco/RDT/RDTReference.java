a8aa26a4ca7241af22a4a99beec9c78ad1fc03a0
Mrbysco <Heeybysco@live.nl>
2016-11-15 19:54:06 +0100

    Version fix

--- src/main/java/com/Mrbysco/RDT/RDTReference.java
+++ src/main/java/com/Mrbysco/RDT/RDTReference.java
@@ -3,7 +3,7 @@ package com.Mrbysco.RDT;
 public class RDTReference {
 	public static final String MOD_ID = "rdt";
 	public static final String MOD_NAME = "Random Decorative Things";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.4";
 	public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 			
 	public static final String CLIENT_PROXY_CLASS = "com.Mrbysco.RDT.proxy.ClientProxy";
