da36adb1df329443150f69dc6826ef9de2f5798a
Mrbysco <Heeybysco@live.nl>
2017-02-26 16:03:12 +0100

    Fixed grass color on butcher village house

--- src/main/java/com/Mrbysco/RDT/proxy/ClientProxy.java
+++ src/main/java/com/Mrbysco/RDT/proxy/ClientProxy.java
@@ -1,15 +1,14 @@
 package com.Mrbysco.RDT.proxy;
 
 import com.Mrbysco.RDT.init.RDTBlocks;
+import com.Mrbysco.RDT.init.RDTColor;
 import com.Mrbysco.RDT.init.RDTItems;
 
 public class ClientProxy extends CommonProxy{
 	
 	@Override
 	public void init() {
-        //NetworkRegistry.INSTANCE.registerGuiHandler(RDTReference.MOD_ID, new GuiProxy());
-		//RDTItems.registerRenders();
-		//RDTBlocks.registerRenders();
+		RDTColor.registerColourHandlers();
 	}
 	
 	
