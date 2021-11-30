1373104701b4d8308bba24205d619ff7c7f71ae7
Mrbysco <Mrbysco@users.noreply.github.com>
2021-11-30 23:23:46 +0100

    Initial port to 1.18 [push]

--- src/main/java/com/mrbysco/rdt/init/RandomRegistry.java
+++ src/main/java/com/mrbysco/rdt/init/RandomRegistry.java
@@ -19,9 +19,9 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class RandomRegistry {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
